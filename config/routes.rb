2cb98f9611161905ad1e74ef45cc8abbe035b41b
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2022-04-19 05:56:44 +0000

    Finalized Feed, Followers and Following tabs and refactored, tab bar

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,15 @@ Rails.application.routes.draw do
   resources :photos
 
   get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
+
+
 
   get ":username" => "users#show", as: :user
+
+
   #root "photos#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
